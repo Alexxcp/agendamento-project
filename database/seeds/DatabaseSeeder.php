adc75751a0151cee3ae11345bae74a079e3e7e3c
Alex Costa <suporte@alexpereira.net.br>
2020-12-10 15:52:02 -0300

    Correcao migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PacientesTableSeeder::class,
-            ContatosTableSeeder::class,
             UsersTableSeeder::class,
         ]);
     }
