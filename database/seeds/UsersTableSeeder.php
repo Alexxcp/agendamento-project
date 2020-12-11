9f268c8890e6ea89fdb4902fb330be3a2abe1fea
Alex Costa <suporte@alexpereira.net.br>
2020-12-10 22:44:21 -0300

    Datatable

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,7 +15,7 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Administrador',
             'email' => 'admin@admin.com',
-            'password' => bcrypt('secret'),
+            'password' => bcrypt('123456'),
             'created_at' => date('Y-m-d H:i:s')
         ]);
 
